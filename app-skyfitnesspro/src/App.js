cd4ae1b958475a2b5a94223040f4f3f43c2d3a47
AnnaIllarionova <annailinas@gmail.com>
2023-12-29 19:35:15 +0300

    fixed bugs

--- app-skyfitnesspro/src/App.js
+++ app-skyfitnesspro/src/App.js
@@ -1,61 +1,12 @@
-import { useState } from "react";
 import * as S from "./App.styled";
-import { WorkoutVideoPage } from "./components/workout-video-page/workout-video-page";
 import { GlobalStyles } from "./global.styled";
-import { MyProgress } from "./progress/progress";
-import { CountedProgress } from "./progress/progress-counted";
+import { WorkOutComponent } from "./components/work-out-component/work-out-component";
 
 function App() {
-  const [isMyProgressOpen, setIsMyProgressOpen] = useState(false);
-  const [isMyProgressCounted, setIsMyProgressCounted] = useState(false);
-  const [isErrorExist, setIsErrorExist] = useState(false);
-  const [progressValue, setProgressValue] = useState("");
-  const [progressValueSecond, setProgressValueSecond] = useState("");
-  const [progressValueThird, setProgressValueThird] = useState("");
-
-  const openModalWindow = () => {
-    setIsMyProgressOpen(true);
-  };
-
-  const closeModalWindow = () => {
-    if (
-      progressValue === "" ||
-      progressValueSecond === "" ||
-      progressValueThird === ""
-    ) {
-      setIsMyProgressOpen(true);
-      setIsErrorExist(true);
-    } else {
-      setIsMyProgressOpen(false);
-      setIsMyProgressCounted(true);
-
-      setTimeout(() => {
-        setIsMyProgressCounted(false);
-      }, 2000);
-    }
-  };
-
   return (
     <S.Wrapper>
       <GlobalStyles />
-      <WorkoutVideoPage
-        progressValue={progressValue}
-        openModalWindow={openModalWindow}
-        progressValueSecond={progressValueSecond}
-        progressValueThird={progressValueThird}
-      />
-      <MyProgress
-        progressValue={progressValue}
-        progressValueSecond={progressValueSecond}
-        progressValueThird={progressValueThird}
-        setProgressValue={setProgressValue}
-        setProgressValueSecond={setProgressValueSecond}
-        setProgressValueThird={setProgressValueThird}
-        isMyProgressOpen={isMyProgressOpen}
-        closeModalWindow={closeModalWindow}
-        isErrorExist={isErrorExist}
-      />
-      <CountedProgress isMyProgressCounted={isMyProgressCounted} />
+     <WorkOutComponent />
     </S.Wrapper>
   );
 }
---
+++ app-skyfitnesspro/src/components/work-out-component/work-out-component.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import { WorkoutVideoPage } from "../workout-video-page/workout-video-page";
+import { MyProgress } from "../progress/progress";
+import { CountedProgress } from "../progress/progress-counted";
+
+export const WorkOutComponent = () => {
+  const [isMyProgressOpen, setIsMyProgressOpen] = useState(false);
+  const [isMyProgressCounted, setIsMyProgressCounted] = useState(false);
+  const [isErrorExist, setIsErrorExist] = useState(false);
+  const [progressValue, setProgressValue] = useState("");
+  const [progressValueSecond, setProgressValueSecond] = useState("");
+  const [progressValueThird, setProgressValueThird] = useState("");
+
+  const openModalWindow = () => {
+    setIsMyProgressOpen(true);
+  };
+
+  const closeModalWindow = () => {
+    if (
+      progressValue === "" ||
+      progressValueSecond === "" ||
+      progressValueThird === ""
+    ) {
+      setIsMyProgressOpen(true);
+      setIsErrorExist(true);
+    } else {
+      setIsMyProgressOpen(false);
+      setIsMyProgressCounted(true);
+
+      setTimeout(() => {
+        setIsMyProgressCounted(false);
+      }, 2000);
+    }
+  };
+
+  return (
+    <>
+      <WorkoutVideoPage
+        progressValue={progressValue}
+        openModalWindow={openModalWindow}
+        progressValueSecond={progressValueSecond}
+        progressValueThird={progressValueThird}
+      />
+      <MyProgress
+        progressValue={progressValue}
+        progressValueSecond={progressValueSecond}
+        progressValueThird={progressValueThird}
+        setProgressValue={setProgressValue}
+        setProgressValueSecond={setProgressValueSecond}
+        setProgressValueThird={setProgressValueThird}
+        isMyProgressOpen={isMyProgressOpen}
+        closeModalWindow={closeModalWindow}
+        isErrorExist={isErrorExist}
+      />
+      <CountedProgress isMyProgressCounted={isMyProgressCounted} />
+    </>
+  );
+};
