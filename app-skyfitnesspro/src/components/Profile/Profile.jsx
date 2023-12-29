0353145b25cf3232828870dfe58d4a170ef6271f
Aleksey8622 <121502314+Aleksey8622@users.noreply.github.com>
2023-12-29 16:38:20 +0300

    Перевел Profile на styled-components

--- app-skyfitnesspro/src/components/Profile/Profile.jsx
+++ app-skyfitnesspro/src/components/Profile/Profile.jsx
@@ -1,52 +1,51 @@
 import React from "react";
-import "./Profile.css";
-
+import * as S from "./ProfileStyle";
 const Profile = () => {
+  // const S.ContainerProfile = S.d.div``;
+  // const S.ContainerProfile = S.d.div``;
   return (
-    <div className="container__profile">
-      <div className="header">
-        <div className="header-logo">
-          <img src="/img/logo-SkyFitnessPro.svg" alt="logo" />
-        </div>
-        <div className="header-profile">
-          <svg className="header-svg">
+    <S.ContainerProfile>
+      <S.Header>
+        <S.HeaderLogo>
+          <S.Img src="/img/logo-SkyFitnessPro.svg" alt="logo" />
+        </S.HeaderLogo>
+        <S.HeaderProfile>
+          <S.HeaderSvg>
             <use xlinkHref="img/icon/sprite.svg#icon-tect-logo"></use>
-          </svg>
-          <select className="heder-select" name="select">
+          </S.HeaderSvg>
+          <S.HeaderSelect name="select">
             <option value={"value1"}>Сергей</option>
             <option value={"value2"}>Алексей</option>
             <option value={"value3"}>Айрат</option>
-          </select>
-        </div>
-      </div>
-      <div className="profile">
-        <div className="heading">
-          <h1 className="profile-heading">Мой профиль</h1>
-          <p className="profile-text">Логин: sergey.petrov96</p>
-          <p className="profile-text">Пароль: 4fkhdj880d</p>
-        </div>
-        <div className="profile-button">
-          <button className="button">Редактировать логин</button>
-          <button className="button">
-            Редактировать пароль
-          </button>
-        </div>
-      </div>
-      <div className="course">
-        <h1>Мои курсы</h1>
-        <div className="course-box">
-          <div className="course-item course-item-1">
-            <button className="button-link">Перейти</button>
-          </div>
-          <div className="course-item course-item-2">
-            <button className="button-link ">Перейти</button>
-          </div>
-          <div className="course-item course-item-3">
-            <button className="button-link button-link-3">Перейти</button>
-          </div>
-        </div>
-      </div>
-    </div>
+          </S.HeaderSelect>
+        </S.HeaderProfile>
+      </S.Header>
+      <S.Profile>
+        <S.Heading>
+          <S.ProfileHeading>Мой профиль</S.ProfileHeading>
+          <S.ProfileText>Логин: sergey.petrov96</S.ProfileText>
+          <S.ProfileText>Пароль: 4fkhdj880d</S.ProfileText>
+        </S.Heading>
+        <S.ProfileButton>
+          <S.Button>Редактировать логин</S.Button>
+          <S.Button>Редактировать пароль</S.Button>
+        </S.ProfileButton>
+      </S.Profile>
+      <S.Course>
+        <S.ProfileHeading>Мои курсы</S.ProfileHeading>
+        <S.CourseBox>
+          <S.CourseItem1>
+            <S.ButtonLink>Перейти</S.ButtonLink>
+          </S.CourseItem1>
+          <S.CourseItem2>
+            <S.ButtonLink>Перейти</S.ButtonLink>
+          </S.CourseItem2>
+          <S.CourseItem3>
+            <S.ButtonLink3>Перейти</S.ButtonLink3>
+          </S.CourseItem3>
+        </S.CourseBox>
+      </S.Course>
+    </S.ContainerProfile>
   );
 };
 
---
+++ app-skyfitnesspro/src/components/Profile/ProfileStyle.js
@@ -0,0 +1,127 @@
+import { styled } from "styled-components";
+
+export const ContainerProfile = styled.div``;
+export const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 30px;
+`;
+export const HeaderLogo = styled.div``;
+export const Img = styled.img``;
+export const HeaderProfile = styled.div`
+  display: flex;
+  column-gap: 9px;
+`;
+export const HeaderSvg = styled.svg`
+  width: 50px;
+  height: 50px;
+  flex-shrink: 0;
+`;
+export const HeaderSelect = styled.select`
+  color: #000;
+  text-align: right;
+  border: 0px;
+  outline: 0px;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 32px;
+  letter-spacing: -0.1px;
+`;
+export const Profile = styled.div``;
+export const Heading = styled.div``;
+export const ProfileHeading = styled.h1`
+  color: #000;
+  font-variant-numeric: lining-nums proportional-nums;
+  font-family: StratosSkyeng;
+  font-size: 48px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 56px;
+  margin-top: 75px;
+`;
+export const ProfileText = styled.p`
+  color: #000;
+  font-variant-numeric: lining-nums proportional-nums;
+  font-family: StratosSkyeng;
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 32px;
+  margin-top: 40px;
+`;
+export const ProfileButton = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 14px;
+  margin-top: 40px;
+`;
+export const Button = styled.button`
+  width: 275px;
+  height: 52px;
+  flex-shrink: 0;
+  border-radius: 46px;
+  background: var(--Palette-Purple-90, #580ea2);
+  color: #fff;
+  font-variant-numeric: lining-nums proportional-nums;
+  font-family: StratosSkyeng;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 24px;
+  letter-spacing: -0.05px;
+`;
+export const Course = styled.div`
+  margin-top: 75px;
+`;
+export const CourseBox = styled.div`
+  display: flex;
+  justify-content: space-between;
+  column-gap: 40px;
+  margin-top: 60px;
+`;
+export const CourseItem = styled.div`
+  width: 360px;
+  height: 480px;
+  position: relative;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+`;
+export const CourseItem1 = styled(CourseItem)`
+  background-image: url("./img/icon-class-yoga.png");
+`;
+export const CourseItem2 = styled(CourseItem)`
+  background-image: url("./img/icon-class-Stretching.png");
+`;
+export const CourseItem3 = styled(CourseItem)`
+  background-image: url("./img/icon-class-Bodyflex.png");
+  width: 330px;
+  height: 435px;
+  margin-top: 24px;
+  box-shadow: -10px 10px 15.999px 0px rgba(0, 0, 0, 0.1),
+    10px -10px 16px 0px rgba(0, 0, 0, 0.1);
+  border-radius: 29.998px;
+`;
+export const ButtonLink = styled.button`
+  width: 136px;
+  height: 43px;
+  position: absolute;
+  left: 40px;
+  bottom: 47px;
+  border-radius: 80px;
+  background: #c7e957;
+  color: #000;
+  text-align: center;
+  font-variant-numeric: lining-nums proportional-nums;
+  font-family: StratosSkyeng;
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 115%;
+  letter-spacing: -0.05px;
+  border: none;
+`;
+export const ButtonLink3 = styled(ButtonLink)`
+  bottom: 22px;
+`;
