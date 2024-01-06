af17536614c7b93c37d6d8d90df7b51bb3b2eda4
BedretdinovAirat <bedretdinov-a@mail.ru>
2024-01-06 15:07:06 +0300

    06.01.2024 - удалили комментарии в файле индекс.джс

--- app-skyfitnesspro/src/index.js
+++ app-skyfitnesspro/src/index.js
@@ -1,13 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import "./index.css";
 import App from "./App.js";
-// import { GlobalStyled } from "./GlobalAppStyle";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
-    {/* <GlobalStyled /> */}
     <App />
   </>
 );
