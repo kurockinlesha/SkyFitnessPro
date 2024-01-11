b5dc7f050f43e22b478b0335cec554f2e4bcfecc
BedretdinovAirat <bedretdinov-a@mail.ru>
2024-01-11 21:09:29 +0300

    11.01.2024 - удалил фиксированную ширину у страниц

--- app-skyfitnesspro/src/AppStyle.js
+++ app-skyfitnesspro/src/AppStyle.js
@@ -3,12 +3,11 @@ import { styled } from "styled-components";
 export const Wrapper = styled.div`
   width: 100%;
   height: 100%;
-  overflow: hidden;  
+  overflow: hidden;
 `;
 
 export const Container = styled.div`
   // max-width: 1920px;
-  height: 100vh;
   margin: 0 auto;
   position: relative;
   padding-left: calc(50% - 580px);
