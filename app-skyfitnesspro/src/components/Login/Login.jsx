1bbaa8b3402df66863a47722acfd6d1d825a345f
Aleksey8622 <121502314+Aleksey8622@users.noreply.github.com>
2024-01-11 20:57:03 +0300

    added modal window

--- app-skyfitnesspro/src/components/Login/Login.jsx
+++ app-skyfitnesspro/src/components/Login/Login.jsx
@@ -1,38 +1,35 @@
 import React from "react";
 
-import * as S from "../Password/LoginPasswordStyle"
-const Login = () => {
- 
+import * as S from "../Password/LoginPasswordStyle";
+const Login = ({ modalActiveLogin, setModalActiveLogin }) => {
   // const S.Wrapper = S.d.div``
 
   return (
     // <S.Wrapper>
     //   <S.ContainerSignup>
-        <S.ModalBlock>
-          <S.ModalFormLoginWigth>
+    <>
+      {modalActiveLogin && (
+        <S.ModalBlock onClick={() => setModalActiveLogin(false)}>
+          <S.ModalFormLoginWigth onClick={(e) => e.stopPropagation()}>
             <S.ModalLogo>
-              <S.ModalLogoImg
-                src="./img/logo-SkyFitnessPro.svg"
-                alt="logo"
-              />
+              <S.ModalLogoImg src="./img/logo-SkyFitnessPro.svg" alt="logo" />
             </S.ModalLogo>
             <S.NewLoginPassword>
-              <S.NewLoginPasswordText>
-                Новый логин:
-              </S.NewLoginPasswordText>
+              <S.NewLoginPasswordText>Новый логин:</S.NewLoginPasswordText>
             </S.NewLoginPassword>
             <S.ModalInput type="text" name="login" placeholder="Логин" />
 
             <S.MdalBtnSignupEnt>
-              <S.MdalBtnSignupEntLink href="/">
-                Сохранить
-              </S.MdalBtnSignupEntLink>
+              <S.MdalBtnSignupEntLink>Сохранить</S.MdalBtnSignupEntLink>
             </S.MdalBtnSignupEnt>
           </S.ModalFormLoginWigth>
         </S.ModalBlock>
-    //   </S.ContainerSignup>
-    // </S.Wrapper>
+      )}
+    </>
   );
+
+  //   </S.ContainerSignup>
+  // </S.Wrapper>
 };
 
 export { Login };
--- app-skyfitnesspro/src/components/Password/LoginPasswordStyle.js
+++ app-skyfitnesspro/src/components/Password/LoginPasswordStyle.js
@@ -18,6 +18,14 @@ export const ModalBlock = styled.div`
   left: calc(50% - (366px / 2));
   top: calc(50% - (439px / 2));
   opacity: 2;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.4);
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 export const ModalFormLogin = styled.form`
   width: 366px;
@@ -39,8 +47,8 @@ export const ModalFormLogin = styled.form`
 `;
 
 export const ModalFormLoginWigth = styled(ModalFormLogin)`
-width: 366px;
-height: 341px;
+  width: 366px;
+  height: 341px;
 `;
 export const ModalLogo = styled.div`
   width: 140px;
