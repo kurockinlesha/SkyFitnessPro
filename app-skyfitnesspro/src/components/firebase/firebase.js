a661435831e4ca0d6ab8a43c09b96fdc72dd2dcf
AnnaIllarionova <annailinas@gmail.com>
2024-01-13 19:24:01 +0300

    Получены данные из firebase

--- app-skyfitnesspro/src/components/firebase/firebase.js
+++ app-skyfitnesspro/src/components/firebase/firebase.js
@@ -1,22 +1,18 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
-// import { initializeApp } from "firebase/app"
 
 const firebaseConfig = {
   apiKey: "AIzaSyDmYZms9AfZn000OA1N6TLdc4VgDZA22is",
   authDomain: "fitness-pro-bf034.firebaseapp.com",
-  databaseURL:
-    "https://fitness-pro-bf034-default-rtdb.europe-west1.firebasedatabase.app/",
+  databaseURL: "https://fitness-pro-bf034-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "fitness-pro-bf034",
   storageBucket: "fitness-pro-bf034.appspot.com",
   messagingSenderId: "193730051673",
-  appId: "1:193730051673:web:02ffefc40615cbf83d3b48",
+  appId: "1:193730051673:web:02ffefc40615cbf83d3b48"
 };
 
-firebase.initializeApp(firebaseConfig);
+export const app = firebase.initializeApp(firebaseConfig);
+console.log(firebase.app().name);
 
-export const db = firebase.firestore();
-
-// export const app = firebase.initializeApp( firebaseConfig );
-
-console.log(db);
+const db = firebase.firestore(app);
+console.log("firestore", db);
--- app-skyfitnesspro/src/routers.jsx
+++ app-skyfitnesspro/src/routers.jsx
@@ -7,8 +7,54 @@ import { WorkOutComponent } from "./components/work-out-component/work-out-compo
 import { Auth } from "./pages/auth/Auth";
 import { Reg } from "./pages/reg/Reg"
 import { Login } from "./components/Login/Login";
+import firebase from "firebase/compat/app";
+import "firebase/compat/database";
+import React, { useEffect, useState} from "react";
+import { app } from "./components/firebase/firebase";
 
 export function AppRoutes() {
+
+  //workouts: наименование курса, видео, упражнения
+  const [workoutsFirebase, setWorkoutsFirebase] = useState([]);
+  //courses: напрввления, описания, наименование на рус и англ, workouts
+  const [coursesFirebase, setCoursesFirebase] = useState([]);
+
+  useEffect(() => {
+    const fb = firebase.database(app);
+    const fetchData = async () => {
+      try {
+        const workoutsRef = fb.ref("workouts");
+        workoutsRef.once("value")
+        .then((snapshot) => {
+          const trainings = Object.values(snapshot.val() || {});
+        setWorkoutsFirebase(trainings);
+      }) }
+    catch (error) {
+        console.error("Error getting documents (workouts): ", error);
+      }
+    };
+    fetchData();
+
+    const fetchCoursesData = async () => {
+      try {
+        const coursesRef = fb.ref("courses");
+        coursesRef.once("value")
+        .then((snapshot) => {
+          const trainings = Object.values(snapshot.val() || {});
+        setCoursesFirebase(trainings);
+      }) }
+    catch (error) {
+        console.error("Error getting documents (courses): ", error);
+      }
+    };
+    fetchCoursesData();
+
+  }, []);
+
+  console.log(workoutsFirebase);
+  console.log(coursesFirebase);
+
+
   return (
     <Routes>
       <Route path="/" element={<MainPage />} />
